2b35f3df6928bd4447bd41a35fcffcc808a7fadd
zhou <zhouhaonan4@gmail.com>
2026-02-26 14:02:40 +0800

    feat: 增加书本示例的增加功能

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,5 +1,13 @@
 const BookInstance = require('../models/bookinstance')
+
+const Book = require('../models/book')
+
 const asyncHandler = require('express-async-handler')
+
+const { body, validationResult } = require('express-validator')
+
+// NOTE: API 变动，废弃
+// const { sanitizeBody } = require('express-validator/filter')
 
 // 显示所有的 BookInstances
 // exports.bookinstance_list = asyncHandler(async (req, res, next) => {
@@ -38,13 +46,106 @@ exports.bookinstance_detail = asyncHandler(async (req, res, next) => {
 
 // 由 GET 显示创建 BookInstance 的表单
 exports.bookinstance_create_get = asyncHandler(async (req, res, next) => {
-  res.send('未实现：BookInstance 创建 GET')
+  // res.send('未实现：BookInstance 创建 GET')
+  const books = await Book.find({}, 'title')
+  if (books.err) {
+    return next(books.err)
+  }
+  res.render('bookinstance_form', {
+    title: 'Create BookInstance',
+    book_list: books,
+  })
 })
 
 // 由 POST 处理创建 BookInstance
-exports.bookinstance_create_post = asyncHandler(async (req, res, next) => {
-  res.send('未实现：BookInstance 创建 POST')
-})
+// exports.bookinstance_create_post = asyncHandler(async (req, res, next) => {
+//   res.send('未实现：BookInstance 创建 POST')
+// })
+// Handle BookInstance create on POST.
+exports.bookinstance_create_post = [
+  // Validate fields.
+  body('book', 'Book must be specified').isLength({ min: 1 }).trim(),
+  body('imprint', 'Imprint must be specified').isLength({ min: 1 }).trim(),
+  body('due_back', 'Invalid date').optional({ checkFalsy: true }).isISO8601(),
+
+  // Sanitize fields.
+  // NOTE: API 变动 start
+  // sanitizeBody('book').trim().escape(),
+  // sanitizeBody('imprint').trim().escape(),
+  // sanitizeBody('status').trim().escape(),
+  // sanitizeBody('due_back').toDate(),
+  // NOTE: API 变动 end
+  body('book').trim().escape(),
+  body('imprint').trim().escape(),
+  body('status').trim().escape(),
+  body('due_back').toDate(),
+
+  // Process request after validation and sanitization.
+  async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req)
+
+    // Create a BookInstance object with escaped and trimmed data.
+    var bookinstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+    })
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values and error messages.
+      // NOTE: 语法问题，exec() 不再回调 start
+      // Book.find({}, 'title').exec(function (err, books) {
+      //   if (err) {
+      //     return next(err)
+      //   }
+      //   // Successful, so render.
+      //   res.render('bookinstance_form', {
+      //     title: 'Create BookInstance',
+      //     book_list: books,
+      //     selected_book: bookinstance.book._id,
+      //     errors: errors.array(),
+      //     bookinstance: bookinstance,
+      //   })
+      // })
+      // NOTE: 语法问题，exec() 不再回调 end
+      const books = await Book.find({}, 'title').exec()
+      if (books.err) {
+        const err = new Error('Book not found')
+        err.status = 404
+        return next(err)
+      }
+      res.render('bookinstance_form', {
+        title: 'Create BookInstance',
+        book_list: books,
+        selected_book: bookinstance.book._id,
+        errors: errors.array(),
+        bookinstance: bookinstance,
+      })
+      return
+    } else {
+      // Data from form is valid.
+      // NOTE: 语法问题，不支持回调函数，使用链式调用 start
+      // bookinstance.save(function (err) {
+      //   if (err) {
+      //     return next(err)
+      //   }
+      //   // Successful - redirect to new record.
+      //   res.redirect(bookinstance.url)
+      // })
+      // NOTE: 语法问题，不支持回调函数，使用链式调用 end
+      bookinstance
+        .save()
+        .then(() => {
+          res.redirect(bookinstance.url)
+        })
+        .catch(err => {
+          return next(err)
+        })
+    }
+  },
+]
 
 // 由 GET 显示删除 BookInstance 的表单
 exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
